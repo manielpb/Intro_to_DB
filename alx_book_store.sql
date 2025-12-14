c8cd253c5dc36215400369ae0b6096c2b5a8709d
maniel <manuelpepr99@icloud.com>
2025-12-14 04:14:23 +0000

    Tables Creation -u

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,9 +1,10 @@
 CREATE DATABASE IF NOT EXISTS alx_book_store;
 
 CREATE TABLE Books (
-    book_id PRIMARY KEY,
+    book_id INT PRIMARY KEY,
     title VARCHAR(130),
-    author_id FOREIGN KEY REFERENCES Authors,
+    author_id,
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
     price DOUBLE,
     publication_date DATE
 );
