8b19064c059e0530dc551796340901ba21248df8
maniel <manuelpepr99@icloud.com>
2025-12-14 03:52:28 +0000

    Tables Creation

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,1 +1,34 @@
 CREATE DATABASE IF NOT EXISTS alx_book_store;
+
+CREATE TABLE Books (
+    book_id PRIMARY KEY,
+    title VARCHAR(130),
+    author_id FOREIGN KEY REFERENCES Authors,
+    price DOUBLE,
+    publication_date DATE
+);
+
+CREATE TABLE Authors(
+    author_id PRIMARY KEY,
+    author_name VARCHAR(215),
+);
+
+CREATE TABLE Customers(
+    customer_id PRIMARY KEY,
+    customer_name VARCHAR(215),
+    email VARCHAR(215)
+    address TEXT,
+);
+
+CREATE TABLE Orders(
+    order_id PRIMARY KEY
+    customer_id FOREIGN KEY REFERENCES Customers,
+    order_date DATE,
+);
+
+CREATE TABLE Order_Details(
+    orderdetailid PRIMARY KEY,
+    order_id FOREIGN KEY REFERENCES Orders,
+    book_id FOREIGN KEY REFERENCES Books,
+    quantity DOUBLE
+)
