b33927cf49fae0aab1b935787daf07b608bde7b4
maniel <manuelpepr99@icloud.com>
2025-12-14 04:08:52 +0000

    Tables Creation -u

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -22,8 +22,10 @@ CREATE TABLE Customers(
 
 CREATE TABLE Orders(
     order_id INT PRIMARY KEY
-    customer_id INT FOREIGN KEY REFERENCES Customers,
+    customer_id INT,
     order_date DATE,
+    FOREIGN KEY (customer_id)
+        REFERENCES Customers(customer_id)
 );
 
 CREATE TABLE Order_Details(
